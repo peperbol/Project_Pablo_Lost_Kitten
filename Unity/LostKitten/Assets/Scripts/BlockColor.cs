69037e902324dc167e73cb0d03024010eb8fef4e
Thessa Moerloos <thessa.moerloos@student.kdg.be>
2015-02-02 16:12:30 +0100

    Block color & color spectrum

---
+++ Unity/LostKitten/Assets/Scripts/BlockColor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum BlockColor {
+
+  // enum aanmaken van alle kleuren van het kleurenspectrum
+
+  Red, Orange, Yellow, Green, Blue, Purple 
+
+
+
+
+
+
+}
---
+++ Unity/LostKitten/Assets/Scripts/ColorSpectrum.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorSpectrum  {
+
+// methode aanmaken: IsAdjacent (blockColor, blockColor)
+// methode aanmaken: GetAdjacentColor(bool, blockColor)
+ //methode aanmaken: GetComplementColor(blockColor)
+
+
+
+
+}
