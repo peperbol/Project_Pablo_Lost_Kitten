d95c3c610c9c750453fae13cf86ad017e92ae232
Thessa Moerloos <thessa.moerloos@student.kdg.be>
2015-02-09 15:56:40 +0100

    update input coltroller :alien:

--- Unity/LostKitten/Assets/Scripts/InputController.cs
+++ Unity/LostKitten/Assets/Scripts/InputController.cs
@@ -1,39 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class Boundary
-{
-  public float xMin, xMax, zMin, zMax;  // zo kunnen we die aangezien we het in een aparte klasse steken, overal aan 
-}
-
+using System.Diagnostics;
 
 
 public class InputController : MonoBehaviour
 {
 
-  public float speed;
-  public Boundary boundary;
+  public float trigger = 0.5f;  //
+  public float stepsPerSecond =  8; // want kat is 8 blokjes lang, dus dan gaat hij 8 blokjes per seconden kunnen doen.
+  private float nextStepTime = 0;
 
 
-
-
-  private void FixedUpdate()
+  private void Update()
   {
+    if (nextStepTime < Time.time)   // Time.time = hoe lang is het spel al aan het lopen/runnen?
+    {
+      nextStepTime = Time.time + 1/stepsPerSecond;  // het volgende tijdspunt, dat je terug een stap mag zetten   =   duratie van het spel +    1/8(hoelang 1 stap duurt)
+    
+      float moveHorizontal = Input.GetAxis("Horizontal");  // waarde tussen -1 en 1    bv: 0.4 naar links
+      float moveVertical = Input.GetAxis("Vertical");
 
-    float moveHorizontal = Input.GetAxis("Horizontal");
-    float moveVertical = Input.GetAxis("Vertical");
+        if (Mathf.Abs(moveHorizontal) > trigger) // dus als je 0.4 naar links gaat, dan ga je nog niet naar links, maar vanaf je move horizontal groter is dan 0.5 gaat hij naar links bewegen
+        {
+          if (moveHorizontal > 0)
+          {
+          }
+          else
+          {
+          }
+        }
+        if (Mathf.Abs(moveVertical) > trigger)//dus als je 0.4 naar boven/onder gaat, gaat het niet mogen, mintsens 0.5 om te bewegen
+        {
+          if (moveVertical > 0)
+          {
 
-                        // Vector3(x,y,z)
-    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);   // nieuwe variabele aanmaken die beschrijft hoe de speler naar onder boven links en rechts gaat
-    rigidbody.velocity = movement*speed;
+          }
+          else
+          {
+        
+          }
+        }
 
-    rigidbody.position = new Vector3    //(x,y,z)
-      //boundary --> zorgt ervoor dat je speler binnen de perken van het spel blijft. en niet u scherm kan verlaten
-      (
-      Mathf.Clamp(rigidbody.position.x, xMin, xMax),    // clamp zorgt ervoor dat je een waarde hebt tussen een min en een max. 
-      0.0f, // 2D je hoeft niet omhoog
-      Mathf.Clamp(rigidbody.position.z, zMin, zMax)
-      );
+    } // einde if time
+
   }
 
 
