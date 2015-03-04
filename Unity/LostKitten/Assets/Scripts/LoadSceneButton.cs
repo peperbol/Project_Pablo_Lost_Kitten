288dcd9f9502bcd16ca8fa3fea181cebeca3340e
peperbol <pepijnwillekens@gmail.com>
2015-03-04 15:52:20 +0100

    startscherm +gc rename :tanabata_tree:

---
+++ Unity/LostKitten/Assets/Scripts/LoadSceneButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadSceneButton : MonoBehaviour
+{
+  public string LevelToLoad;
+  public void onClick()
+  {
+    Debug.Log("hi");
+	Application.LoadLevel(LevelToLoad);
+	}
+	
+}
