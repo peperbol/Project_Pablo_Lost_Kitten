7c0d99365c8bf19243d9c96de51951645e940303
Phedra Moerloos <phedra.moerloos@student.kdg.be>
2015-03-06 15:19:07 +0100

    Gamecontroller edit, LoadLevel aangemaakt
    Gamecontroller currentLevelName variabele, LoadLevel in Scene
    aangemaakt, Axis verwijdert (niet meer nodig)

---
+++ Unity/LostKitten/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	  new Level(FileReaderWriter.GetLevelTemplate(GameController.CurrentLevelName)); //In FileReaderWriter hebben we een methode GetLevelTemplate waar je de naam van het Level dat je moet laden meegeeft, hij zoekt dan naar die file en laad het Level.
+
+	}
+	
+}
